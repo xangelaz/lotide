38c59a20198645f2ed8a615e66f842c2250acc4a
Angela Zhou <a03zhou@gmail.com>
2023-05-30 19:13:58 -0400

    final after linting

--- tail.js
+++ tail.js
@@ -7,17 +7,17 @@ const assertEqual  = function(actual, expected) {
 };
 
 const tail = function(array) {
-  const newArray = []
-  for(let i = 1; i < array.length; i++) {
-    newArray.push(array[i])
+  const newArray = [];
+  for (let i = 1; i < array.length; i++) {
+    newArray.push(array[i]);
   }
-  return newArray
+  return newArray;
 };
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
-// some tests 
+// some tests
 assertEqual(tail([5,6,7]).length, 2);
 assertEqual(tail([3]).length, 0);
 assertEqual(tail([]).length, 0);
