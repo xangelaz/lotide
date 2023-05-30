25a15fc3732d5a192237f1916c7429b84aa45c52
Angela Zhou <a03zhou@gmail.com>
2023-05-30 19:08:32 -0400

    final tail assignment

---
+++ tail.js
@@ -0,0 +1,23 @@
+const assertEqual  = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    return;
+  }
+  console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const tail = function(array) {
+  const newArray = []
+  for(let i = 1; i < array.length; i++) {
+    newArray.push(array[i])
+  }
+  return newArray
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+// some tests 
+assertEqual(tail([5,6,7]).length, 2);
+assertEqual(tail([3]).length, 0);
+assertEqual(tail([]).length, 0);
