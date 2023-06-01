afa6d51c483446a72db01dd64fbc8bc46302ddcc
Angela Zhou <a03zhou@gmail.com>
2023-06-01 03:34:22 -0400

    countLetters assignment final

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const assertEqual  = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    return;
+  }
+  console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const sentence = "lighthouse in the house"
+
+const countLetters = function(sentence) {
+  let letter = {}
+  for (let i of sentence) {
+    if (i !== " ") {
+      if(letter[i] !== undefined) {
+        letter[i] ++ 
+      } 
+      else {
+        letter[i] = 1
+      }
+  }
+}
+console.log(letter)
+}
+
+countLetters(sentence)
