f6d455aa2a9a2b8539e593d3c95de0579c540ef6
Angela Zhou <a03zhou@gmail.com>
2023-06-02 01:30:13 -0400

    map function with 3 tests

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,47 @@
-const assertArraysEqual = function(array1, array2) {
+const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
-    return;
-  } else {
+    return false
+  }
+  else {
     for (let i = 0; i < array1.length; i++) {
       if (array1[i] !== array2[i]) {
-        console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
-        return;
+        return false;
       }
     }
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+    return true
   }
+}
+
+const assertArraysEqual = function(array1, array2) {
+  if (!eqArrays(array1, array2)) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+    return;
+  }
+  console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
 };
+
+// const assertArraysEqual = function(array1, array2) {
+//   if (eqArrays(array1, array2)) {
+//     console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);  
+//     return;
+//   }
+//   console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+// };
+
+// const assertArraysEqual = function(array1, array2) {
+//   if (array1.length !== array2.length) {
+//     console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+//     return;
+//   } else {
+//     for (let i = 0; i < array1.length; i++) {
+//       if (array1[i] !== array2[i]) {
+//         console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+//         return;
+//       }
+//     }
+//     console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+//   }
+// };
 
 (assertArraysEqual([1, 2, 3], [1, 2, 3]));
 (assertArraysEqual([1, 2, 4], [1, 2, 3]));
--- letterPositions.js
+++ letterPositions.js
@@ -1,24 +1,35 @@
 const letterPositions = function(sentence) {
   const results = {};
-  // logic to update results here
-  return results;
+  for (let i = 0; i < sentence.length - 1; i++) {
+    results[i]
+  }
+  console.log(results);
 };
 
-const assertArraysEqual = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
-    return;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
-        return;
-      }
-    }
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-  }
-};
+
+
+/* i think the output will be something like:
+h: 0
+e: 1
+l: 2, 3
+o: 4
+*/
+
+// const assertArraysEqual = function(array1, array2) {
+//   if (array1.length !== array2.length) {
+//     console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+//     return;
+//   } else {
+//     for (let i = 0; i < array1.length; i++) {
+//       if (array1[i] !== array2[i]) {
+//         console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+//         return;
+//       }
+//     }
+//     console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+//   }
+// };
 
 const sentence = "hello"
 
-(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+// assertArraysEqual(letterPositions("hello").h, [0]);
