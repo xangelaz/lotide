dfc56a285866a0ab2ca693ba1fd30f766e1e5e4b
Angela Zhou <a03zhou@gmail.com>
2023-06-05 21:51:29 -0400

    eqArraysTest created

---
+++ test/eqArraysTest.js
@@ -0,0 +1,11 @@
+const eqArrays = require('../eqArrays');
+const assertEqual = require('../assertEqual');
+
+
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), false); 
+assertEqual(eqArrays([1, 9, 3], [1, 2, 3]), false); 
