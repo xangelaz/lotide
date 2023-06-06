274f77e7b1ba5a25fe01e8ee83e4bc8b079ad1be
Angela Zhou <a03zhou@gmail.com>
2023-06-06 00:57:06 -0400

    index created w 5 functions

---
+++ index.js
@@ -0,0 +1,14 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+};
