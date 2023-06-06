ded7c3a871f541d1da6ff1f426eda6f17c3852d6
Angela Zhou <a03zhou@gmail.com>
2023-06-05 22:24:17 -0400

    refactored with export

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,4 @@
-const eqArrays = require('../eqArrays');
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(array1, array2) {
   if (!eqArrays(array1, array2)) {
@@ -10,6 +10,3 @@ const assertArraysEqual = function(array1, array2) {
 
 module.exports = assertArraysEqual;
 
-(assertArraysEqual([1, 2, 3], [1, 2, 3]));
-(assertArraysEqual([1, 2, 4], [1, 2, 3]));
-(assertArraysEqual([1, 2, 3], [1, 2, 3, 5]));
