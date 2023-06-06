03c3739c2c1985466a7441b0afa7591c88acd69f
Angela Zhou <a03zhou@gmail.com>
2023-06-05 22:23:56 -0400

    assertArraysEqualTest created

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -2,6 +2,6 @@ const assertArraysEqual = require('../assertArraysEqual');
 const eqArrays = require('../eqArrays');
 
 
-assertArraysEqual(assertEq([1, 2, 3], [1, 2, 3]));
-(assertArraysEqual([1, 2, 4], [1, 2, 3]));
-(assertArraysEqual([1, 2, 3], [1, 2, 3, 5]));
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 4], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [1, 2, 3, 5]);
