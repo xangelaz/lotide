4bc893b507642d1dab7012474541297355426e06
Angela Zhou <a03zhou@gmail.com>
2023-06-06 02:28:59 -0400

    findKeys complete

---
+++ findKey.js
@@ -0,0 +1,41 @@
+const assertEqual  = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    return;
+  }
+  console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKey = function(object, callback) {
+  for (let key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+  return undefined;
+}
+
+
+// console.log(findKey({
+//   "Blue Hill": { stars: 1 },
+//   "Akaleri":   { stars: 3 },
+//   "noma":      { stars: 2 },
+//   "elBulli":   { stars: 3 },
+//   "Ora":       { stars: 2 },
+//   "Akelarre":  { stars: 3 }
+// }, x => x.stars === 2));
+
+const things = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 },
+}
+const callback = x => x.stars === 2;
+
+//test cases 
+assertEqual(findKey(things, callback), "noma");
+assertEqual(findKey(things, callback), "notnoma");
+
