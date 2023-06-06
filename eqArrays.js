c8e411458687cbe0886cbe48e2b7783fff396f72
Angela Zhou <a03zhou@gmail.com>
2023-06-05 21:51:55 -0400

    refactored with export

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual  = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-    return;
-  }
-  console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-};
-
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false
@@ -20,9 +12,4 @@ const eqArrays = function(array1, array2) {
   }
 }
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), false); 
-assertEqual(eqArrays([1, 9, 3], [1, 2, 3]), false); 
+module.exports = eqArrays; 
