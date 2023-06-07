b1a81e84f5165232e25f69031380d95f9d5d8aa5
Angela Zhou <a03zhou@gmail.com>
2023-06-07 12:50:14 -0400

    linted

--- middle.js
+++ middle.js
@@ -4,12 +4,12 @@ const assertArraysEqual = require('./assertArraysEqual');
 const middle = function(array) {
   if (array.length < 3) {
     return [];
-  } 
+  }
   let i = Math.ceil(array.length / 2 - 1);
   if (array.length % 2 === 0) {
     return [array[i], array[i + 1]];
   }
   return [array[i]];
-}
+};
 
 module.exports = middle;
