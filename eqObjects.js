01db7fe7f6af5e09a2d2a8d6b54c3a78bf47f2cc
Angela Zhou <a03zhou@gmail.com>
2023-07-10 16:36:39 -0400

    imported eqArrays and assertEqual

--- eqObjects.js
+++ eqObjects.js
@@ -1,26 +1,5 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false
-  }
-  else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    return true
-  }
-}
-
-
-const assertEqual  = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-    return;
-  }
-  console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-};
-
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
 
 const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
@@ -28,15 +7,19 @@ const eqObjects = function(object1, object2) {
   }
   for (let key in object1) {
     if (Array.isArray(object1[key]) === true && Array.isArray(object2[key] === true)) {
-      for (let i = 0; i < Object.keys(object1).length; i++)
-      if (object1[i] !== object2[i]) {
-        return false; 
+      eqArrays(Object.keys(object1), Object.keys(object2) === true);
+    } else {
+      for (let i = 0; i < Object.keys(object1).length; i++) {
+        if (object1[i] !== object2[i]) {
+          return false; 
+        }
       }
     }
   }
   return true;
 };
 
+//test code:
 const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
 const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
 eqObjects(multiColorShirtObject  , anotherMultiColorShirtObject); // => true
