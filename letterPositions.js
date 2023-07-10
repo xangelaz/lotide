ff55e598bb41b04d8a4317d35fdfdf4ce1516df6
Angela Zhou <a03zhou@gmail.com>
2023-07-10 15:28:25 -0400

    removed dead code, imported assertArraysEqual

--- letterPositions.js
+++ letterPositions.js
@@ -1,3 +1,5 @@
+const assertArraysEqual = require('./assertArraysEqual');
+
 const letterPositions = function(sentence) {
   const results = {};
   for (let i = 0; i < sentence.length; i++) {
@@ -14,35 +16,7 @@ const letterPositions = function(sentence) {
 console.log(letterPositions("lighthouse in the house"));
 
 
-// const r = { l: 0 }
-// for (let key in r) {
-//   console.log(r[key])
-// }
-// console.log()
-// console.log()
-
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-}
-
-const assertArraysEqual = function(array1, array2) {
-  if (!eqArrays(array1, array2)) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
-    return;
-  }
-  console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
-};
-
+// test code:
 const sentence = "lighthouse in the house"
 
 assertArraysEqual(letterPositions("hello").h, [0]);
