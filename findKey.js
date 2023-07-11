a34092d5e39cc9a085d3d64a07dfb0c94e68cff3
Angela Zhou <a03zhou@gmail.com>
2023-07-10 20:17:52 -0400

    imported assertEqual

--- findKey.js
+++ findKey.js
@@ -1,10 +1,4 @@
-const assertEqual  = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-    return;
-  }
-  console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-};
+const assertEqual = require('./assertEqual');
 
 const findKey = function(object, callback) {
   for (let key in object) {
@@ -15,16 +9,7 @@ const findKey = function(object, callback) {
   return undefined;
 };
 
-
-// console.log(findKey({
-//   "Blue Hill": { stars: 1 },
-//   "Akaleri":   { stars: 3 },
-//   "noma":      { stars: 2 },
-//   "elBulli":   { stars: 3 },
-//   "Ora":       { stars: 2 },
-//   "Akelarre":  { stars: 3 }
-// }, x => x.stars === 2));
-
+//test code:
 const things = {
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
@@ -33,9 +18,7 @@ const things = {
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 },
 };
+
 const callback = x => x.stars === 2;
 
-//test cases
 assertEqual(findKey(things, callback), "noma");
-assertEqual(findKey(things, callback), "notnoma");
-
